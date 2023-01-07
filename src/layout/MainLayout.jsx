3c227ba1138b0882f8b2e80fa623b45106995f7d
galihhhp <pangestu.galihhhp@gmail.com>
2023-01-07 21:55:01 +0700

    ui improvement

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,8 +3,8 @@ import AddGroupModal from 'components/modals/AddGroupModal';
 const MainLayout = ({ children }) => {
   return (
     <div className="h-screen">
-      <nav className="flex items-center gap-2 h-20 pl-10 border-b border-b-black">
-        <h1>Product Roadmap</h1>
+      <nav className="flex items-center gap-[10px] py-[18px] px-[20px] border-b border-b-borderdash">
+        <h1 class="text-textblack font-bold text-[18px]">Product Roadmap</h1>
         <AddGroupModal />
       </nav>
       <div className="main-layout__content">{children}</div>
