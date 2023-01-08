9d6a584cdddcfdbc8d48819bcd98c39eccd714de
galihhhp <pangestu.galihhhp@gmail.com>
2023-01-08 10:16:10 +0700

    improvement: input ui

--- src/components/inputs/TextArea.jsx
+++ src/components/inputs/TextArea.jsx
@@ -7,7 +7,7 @@ const TextArea = ({ label, name, value, onChange, error, placeholder }) => {
         name={name}
         placeholder={placeholder}
         onChange={onChange}
-        className="pt-[8px] pb-[8px] pl-[16px] pr-[16px] rounded-[8px] h-[88px] border-2 border-neutral-400"
+        className="pt-[8px] pb-[8px] pl-[16px] pr-[16px] rounded-[8px] h-[88px] border-2 border-neutral-400 focus:border-primary active:border-primary focus:outline-none text-textdark"
         style={{ resize: 'none' }}
       />
       {error && <p className="text-red-500">{error}</p>}
